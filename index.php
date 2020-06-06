ff55ea94969c8912f673bdf39fb760ac2353c5db
fahad1403 <fahadpatel1403@gmail.com>
2020-06-07 03:53:13 +0530

    Initial Commmit

---
+++ index.php
@@ -0,0 +1,125 @@
+<?php
+
+    include 'logic.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+
+    <!-- Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
+    <!-- Font Awesome -->
+    <script src="https://kit.fontawesome.com/996973c893.js" crossorigin="anonymous"></script>
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Baloo+Thambi+2:wght@400;500;600;700;800&display=swap" rel="stylesheet">
+
+    <link rel="icon" type="image/png" href="world.png"/>
+    <title>Covid-19 Tracker</title>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg  navbar-dark bg-dark">
+		<a class="navbar-brand" href="index.php" style="font-size: 28px;">Tracker</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+		  <span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarText">
+		  <ul class="navbar-nav mr-auto">
+			<li class="nav-item active">
+			  <a class="nav-link" href="precautions.php" style="font-size: 23px;"> Precautions <span class="sr-only">(current)</span></a>
+			</li>
+			<li class="nav-item">
+			  <a class="nav-link" href="#"  style="font-size: 23px;"> Contact </a>
+			</li>
+		  </ul>
+		</div>
+	  </nav>
+    
+    <div class="container-fluid bg-light p-5 text-center my-3">
+        <h1 class="">Covid-19 Tracker</h1>
+        <h5 class="text-muted">A project to keep track of all the COVID-19 cases around the world.</h5>
+    </div>
+
+    <div class="container my-5">
+        <div class="row text-center">
+            <div class="col-4 text-warning">
+                <h5>Confirmed</h5>
+                <?php echo $total_confirmed;?>
+            </div>
+            <div class="col-4 text-success">
+                <h5>Recovered</h5>
+                <?php echo $total_recovered;?>
+            </div>
+            <div class="col-4 text-danger">
+                <h5>Deceased</h5>
+                <?php echo $total_deaths;?>
+            </div>
+        </div>
+    </div>
+
+<!-- Search box
+
+<div class="container">
+<div class="input-group md-form form-sm form-1 pl-0">
+  <div class="input-group-prepend">
+    <span class="input-group-text cyan lighten-2" id="basic-text1"><i class="fas fa-search text-white"
+        aria-hidden="true"></i></span>
+  </div>
+  <input class="form-control my-0 py-1" type="text" id="search" placeholder="Search" aria-label="Search">
+</div>
+</div>
+<br>
+-->
+
+    <div class="container-fluid">
+        <div class="table-responsive">
+            <table class="table">
+                <thead class="thead-dark">
+                    <tr>
+                        <th scope="col">Countries</th>
+                        <th scope="col">Confirmed</th>
+                        <th scope="col">Recovered</th>
+                        <th scope="col">Deaths</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        foreach($data as $key => $value){
+                            $increase = $value[$days_count]['confirmed'] - $value[$days_count_prev]['confirmed'];
+                    ?>
+                        <tr>
+                            <th scope="row"><?php echo $key?></th>
+                            <td>
+                                <?php echo $value[$days_count]['confirmed'];?>
+                                <?php if($increase != 0){ ?>
+                                    <small class="text-danger pl-3"><i class="fas fa-arrow-up"></i><?php echo $increase;?></small>  
+                                <?php } ?>    
+                            </td>
+                            <td><?php echo $value[$days_count]['recovered'];?></td>
+                            <td><?php echo $value[$days_count]['deaths'];?></td>
+                        </tr>
+                    <?php }?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    <footer class="footer mt-auto py-3 bg-light">
+        <div class="container text-center">
+            <span class="text-muted">Copyright &copy;2020, <a href="https://fahadpatel.netlify.app/" target="_blank">Fahad Patel</a></span>
+        </div>
+    </footer>
+
+</body>
+</html>
---
+++ precautions.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+
+    <!-- Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
+    <!-- Font Awesome -->
+    <script src="https://kit.fontawesome.com/996973c893.js" crossorigin="anonymous"></script>
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Baloo+Thambi+2:wght@400;500;600;700;800&display=swap" rel="stylesheet">
+</head>
+<body>
+
+	<nav class="navbar navbar-expand-lg  navbar-dark bg-dark">
+		<a class="navbar-brand" href="index.php" style="font-size: 28px;">Tracker</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+		  <span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarText">
+		  <ul class="navbar-nav mr-auto">
+			<li class="nav-item active">
+			  <a class="nav-link" href="precautions.html" style="font-size: 23px;"> Precautions <span class="sr-only">(current)</span></a>
+			</li>
+			<li class="nav-item">
+			  <a class="nav-link" href="#"  style="font-size: 23px;"> Contact </a>
+			</li>
+		  </ul>
+		</div>
+	  </nav>
+
+
+	<div class="container-fluid bg-light p-3 text-center my-3">
+        <h1 class="">Precautions for Covid-19</h1>
+    </div>
+	<div class="container">
+			<div class="card-body bg-white"><h2>Know How it Spreads</h2></div>
+			<div class="row">
+				<div class="d-none d-md-block cdc-viewports"><img src="images/sneezingwoman.png" class="img-fluid" alt="Illustration: woman sneezing on man" title=""></div>
+				<div class="card-body bg-white">
+				<ul>
+				<li>There is currently no vaccine to prevent coronavirus disease 2019 (COVID-19).</li>
+				<li><strong>The best way to prevent illness is to avoid being exposed to this virus.</strong></li>
+				<li>The virus is thought to spread mainly from person-to-person.
+				<ul>
+				<li>Between people who are in close contact with one another (within about 6 feet).</li>
+				<li>Through respiratory droplets produced when an infected person coughs, sneezes or talks.</li>
+				<li>These droplets can land in the mouths or noses of people who are nearby or possibly be inhaled into the lungs.</li>
+				<li>Some recent studies have suggested that COVID-19 may be spread by people who are not showing symptoms.</li>
+				</ul>
+				</li>
+				</ul>
+				</div>
+			</div>
+		
+
+			<div class="card bl-3 bl-primary mb-3"><div class="card-body bg-gray-l2"><p><strong>More details:</strong> <a href="https://www.who.int/news-room/commentaries/detail/modes-of-transmission-of-virus-causing-covid-19-implications-for-ipc-precaution-recommendations">How COVID-19 Spreads</a></p>
+			</div></div>
+			</div>
+
+			
+			<div class="row">
+			<div class="card-body bg-white text-success text-center" ><h1>Everyone Should</h1></div>
+			</div>
+			
+			<br>
+
+			<div class="container">
+				<div class="row">
+					<div class="d-none d-md-block cdc-viewports"><img src="images/protect-wash-hands.png" class="img-fluid" alt="Illustration: washing hands with soap and water" title="protect-wash-hands"></div>
+					<div class="card-body bg-white"><h3>Clean your hands often</h3>
+						<div class="card-body bg-white">
+							<ul>
+							<li>Wash your hands often with soap and water for at least 20 seconds especially after coughing, or sneezing.</li>
+							<li>If soap and water are not readily available, <strong>use a hand sanitizer that contains at least 60% alcohol</strong>.</li>
+							<li><strong>Avoid touching</strong> <strong>your eyes, nose, and mouth</strong> with unwashed hands.</li>
+							</ul>
+					    </div>
+					    </div>
+					</div>
+					<br><br>
+					<div class="container">
+					    <div class="row">
+					    	
+							<div class="d-none d-md-block cdc-viewports"><img src="images/protect-quarantine.png" class="img-fluid" alt="Illustration: Woman quarantined to her home" title="protect-quarantine"></div>
+							<div class="card-body bg-white"><h3>Avoid close contact</h3>
+							<ul>
+							<li><strong>Avoid close contact</strong> with people who are sick</li>
+							<li><a href="https://www.whitehouse.gov/wp-content/uploads/2020/03/03.16.20_coronavirus-guidance_8.5x11_315PM.pdf" target="new" class="tp-link-policy" data-domain-ext="gov"><strong>Stay home as much as possible</strong>.<span class="sr-only">pdf icon</span><span class="fi cdc-icon-pdf x16 fill-pdf" aria-hidden="true"></span><span class="file-details"></span><span class="sr-only">external icon</span><span class="fi cdc-icon-external x16 fill-external" aria-hidden="true"></span></a> </li>
+							<li>Put <strong>distance between yourself and other</strong> <strong>people</strong>.</li>
+							<li>Remember that some people without symptoms may be able to spread virus.</li>
+							<li>Keeping distance from others is especially important for people who are at higher risk of getting very sick</a>.</li>
+							</ul>
+							</div>
+					    </div>
+			</div>
+			<br>
+			<div class="container">
+				<div class="row">
+					<div class="d-none d-md-block cdc-viewports"><img src="images/cloth-face-cover.png" class="img-fluid" alt="Person with cloth face covering" title="cloth-face-cover"></div>
+					<div class="card-body bg-white"><h3>Cover your mouth and nose with a cloth </h3>
+					<ul>
+					<li>You could spread COVID-19 to others even if you do not feel sick.</li>
+					<li><strong>Everyone should wear a face mask and cover when they have to go out in public</strong></li>
+					<li>Cloth <strong>face coverings should not be placed on young children under age 2</strong>,or anyone who has trouble breathing</li>
+					<li>The cloth face cover is meant to protect other people in case you are infected.</li>
+					<li>Do NOT use a facemask meant for a healthcare worker.</li>
+					<li>Continue to <strong>keep about 6 feet between yourself and others.</strong></li>
+					</ul>
+					</div>
+				</div>
+				
+			</div>
+			<br>
+			<div class="container">
+				<div class="row">
+					<div class="d-none d-md-block cdc-viewports"><img src="images/COVIDweb_09_clean.png" class="img-fluid" alt="cleaning a counter" title="COVIDweb_09_clean"></div>
+					<div class="card-body bg-white"><h3>Clean and disinfect</h3>
+					<ul class="false">
+					<li><strong>Clean touched surfaces daily</strong>.This includes tables, doorknobs, light switches, countertops, handles, desks,etc.</li>
+					<li><strong>If surfaces are dirty, clean them.</strong> Use detergent or soap and water prior to disinfection.</li>
+					<li><strong>Then, use a household disinfectant.</strong> Most common <a class="tp-link-policy tp-link-policy" href="https://www.epa.gov/pesticide-registration/list-n-disinfectants-use-against-sars-cov-2" data-domain-ext="gov">EPA-registered household disinfectant<span class="sr-only">external icon</span><span class="fi cdc-icon-external x16 fill-external" aria-hidden="true"></span></a> will work.</li>
+					</ul>
+					</div>
+				</div>
+			</div>
+</body>
+</html>
